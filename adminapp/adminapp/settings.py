021a809b7d81c8312007a1c294eba890ff0fbfe8
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-01-23 17:28:14 +0700

    init

--- adminapp/adminapp/settings.py
+++ adminapp/adminapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp.apps.MainappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'adminapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mainappdb',
+        'USER': 'root',
+        'PASSWORD': '09102002anha',
+        'HOST': '',
     }
 }
 
