47a1d26af4b172aeb26193c759417a753041a17b
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-02-08 17:15:10 +0700

    update

--- adminapp/mainapp/views.py
+++ adminapp/mainapp/views.py
@@ -80,7 +80,8 @@ class UserViewSet(viewsets.ViewSet,
         User.objects.filter(pk=request.user.id).update(first_name=request.data.get('first_name'),
                                                        last_name=request.data.get('last_name'),
                                                        email=request.data.get('email'),
-                                                       day_of_birth=request.data.get('day_of_birth'))
+                                                       day_of_birth=request.data.get('day_of_birth'),
+                                                       avatar=request.data.get('avatar'))
         return Response(self.serializer_class(User.objects.get(pk=request.user.id)).data,
                         status=status.HTTP_200_OK)
 
